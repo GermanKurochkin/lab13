c955ee0c59e576267b4a6837aa6515d6c7345152
Герман Курочкин <ggkurochkin@edu.hse.ru>
2020-05-30 14:08:30 +0500

    Добавьте файлы проекта.

---
+++ Collection.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab13
+{
+    public class MyCollection : List<Engine>, IEnumerable
+    {
+        public new int Count
+        {
+            get
+            {
+                return list.Count;
+            }
+        }
+        List<Engine> list;
+
+        public new Engine this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > Count)
+                {
+                    Console.WriteLine("Неверный индекс");
+                    return new Engine();
+                }
+                else return list[index];
+            }
+            set
+            {
+                if (index < 0 || index > Count)
+                {
+                    Console.WriteLine("Неверный индекс");
+                }
+                else list[index] = value;
+            }
+        }
+        public MyCollection()
+        {
+            list = new List<Engine>();
+        }
+        public MyCollection(int size)
+        {
+            list = new List<Engine>();
+            Engine eng = new Engine();
+            for (int i = 0; i < size; i++)
+            {
+                list.Add(eng.MakeRandom());
+            }
+        }
+        public MyCollection(MyCollection collection)
+        {
+            list = collection.list;
+        }
+
+        public void Add()
+        {
+            Engine eng = new Engine();
+            list.Add(eng.MakeRandom());
+        }
+        public bool Delete(int position)
+        {
+            if (list.Count < position || position < 0)
+            {
+                return false;
+            }
+            else
+            {
+                list.RemoveAt(position);
+                return true;
+            }
+
+        }
+        public new void Sort()
+        {          
+            list.Sort();
+        }
+        new public void Clear()
+        {
+            list.Clear();
+        }
+        public void Show()
+        {
+            foreach (Engine item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public new IEnumerator GetEnumerator()
+        {
+            foreach (var item in list)
+            {
+                yield return item;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator();
+        }
+
+    }
+
+    public delegate void CollectionHandler(object source, CollectionHandlerEventArgs args);
+    public class MyNewCollection : MyCollection
+    {
+        public string Name { get; set; }
+        public List<Engine> list;
+        public MyNewCollection()
+        {
+            list = new List<Engine>();
+        }
+        public MyNewCollection(int size)
+        {
+            list = new List<Engine>(size);
+            Engine element = new Engine();
+            for (int i = 0; i < size; i++)
+            {
+           
+                list.Add(element.MakeRandom());
+            }
+        }
+        public MyNewCollection(MyNewCollection collection)
+        {
+            list = collection.list;
+        }
+
+        public void AddRandom()
+        {
+            Engine element = new Engine();
+            element = element.MakeRandom();
+            list.Add(element);
+            OnCollectionCountChanged(this, new CollectionHandlerEventArgs(this.Name, "Add", list[list.IndexOf(element)]));
+           
+        }
+        new public void Add(Engine elem)
+        {
+            list.Add(elem);
+            OnCollectionCountChanged(this, new CollectionHandlerEventArgs(this.Name, "Add", list[list.IndexOf(elem)]));
+        
+        }
+        public bool Remove(int position)
+        {
+            if (list.Count < position || position < 0)
+            {
+                return false;
+            }
+            else
+            {
+                OnCollectionCountChanged(this, new CollectionHandlerEventArgs(this.Name, "Delete", list[position]));
+           
+                list.RemoveAt(position);
+                return true;
+            }
+        }
+        public void ChangeValue(int position)
+        {
+            if (list.Count < position || position < 0)
+            {
+                return ;
+            }
+            Engine element = new Engine();
+            OnCollectionReferenceChanged(this, new CollectionHandlerEventArgs(this.Name, "Changed", list[position]));
+            list[position] = element.MakeRandom() ;
+        }
+
+      
+        public new Engine this[int index]
+        {
+            get
+            {
+                return list[index];
+            }
+            set
+            {
+                OnCollectionReferenceChanged(this, new CollectionHandlerEventArgs(this.Name, "Changed", list[index]));
+                list[index] = value;
+            }
+        }
+
+
+       
+        public event CollectionHandler CollectionCountChanged;
+     
+        public event CollectionHandler CollectionReferenceChanged;
+
+
+        public void OnCollectionCountChanged(object source, CollectionHandlerEventArgs args)
+        {
+            if (CollectionCountChanged != null)
+            {
+                CollectionCountChanged(source, args);
+            }
+        }
+        public void OnCollectionReferenceChanged(object source, CollectionHandlerEventArgs args)
+        {
+            if (CollectionReferenceChanged != null)
+            {
+                CollectionReferenceChanged(source, args);
+            }
+        }
+     
+        public void Show()
+        {
+            Console.WriteLine($"Коллекция {Name}:");
+            foreach (var item in this.list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    public class CollectionHandlerEventArgs : System.EventArgs
+    {
+        public string NameCollection { get; set; }
+        public string ChangeType { get; set; }
+        public object ObjectData { get; set; }
+
+        public CollectionHandlerEventArgs(string name, string type, object obj)
+        {
+            NameCollection = name;
+            ChangeType = type;
+            ObjectData = obj;
+        }
+
+        public override string ToString()
+        {
+            return "Имя коллекции: " + NameCollection + "\nОбъект: " + ObjectData + " - " + ChangeType;
+        }
+    }
+
+   
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace lab13
+{
+    class Program
+    {
+        static MyCollection mc1 = new MyCollection();
+
+        static void Add()
+        {
+            Console.WriteLine("Введите количество добавляемых элементов");
+            int num = InputNum(20);
+            for (int i = 0; i < num; i++)
+                mc1.Add();
+        }
+        
+        static void Delete()
+        {
+            Console.WriteLine("Введите место удаления элементов");
+            int place = InputNum(mc1.Count + 1);
+            mc1.Delete(place);
+        }
+        static int InputMenu(int maxNum)
+        {
+            int menu;
+            string input;
+            bool ok;
+            do
+            {
+                input = Console.ReadLine();
+                ok = int.TryParse(input, out menu);
+                if (!ok) Console.WriteLine("Некорректный ввод");
+                else if (menu < 0 || menu > maxNum) Console.WriteLine($"Некорректный ввод.Выберите вариант от 0 до {maxNum} из меню");
+            } while (!ok || menu < 0 || menu > maxNum);
+
+            return menu;
+        }
+
+        static int InputNum(int maxNum)
+        {
+            int num;
+            string input;
+            bool ok;
+            do
+            {
+                input = Console.ReadLine();
+                ok = int.TryParse(input, out num);
+                if (!ok) Console.WriteLine("Некорректный ввод");
+                else if (num < 0 || num > maxNum) Console.WriteLine($"Некорректный ввод. Введите число не больше {maxNum}");
+            } while (!ok || num < 0 || num > maxNum);
+
+            return num;
+
+        }
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int menu = 10;
+
+
+            while (menu != 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("1. Демонстрация коллекции");
+                Console.WriteLine("2. Часть №2");
+                Console.WriteLine("0.Выход");
+                Console.ResetColor();
+                menu = InputMenu(2);
+                mc1 = new MyCollection(10);
+
+
+                if (menu == 0) break;
+                else
+                {
+                    int menuNext = 10;
+
+                    switch (menu)
+                    {
+                        #region task1
+                        case 1:
+
+                            while (menuNext != 0)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine("1.Показать коллекцию");
+                                Console.WriteLine("2.Добавить элементы");
+                                Console.WriteLine("3.Удалить элементы");
+                                Console.WriteLine("4.Сортировка");
+                                Console.WriteLine("5.Удалить коллекцию");
+                                Console.WriteLine("6.Длина");
+                                Console.WriteLine("0.Назад");
+                                Console.ResetColor();
+                                menuNext = InputMenu(6);
+                             
+                                if (menuNext == 0) break;
+                                else
+                                {
+                                    switch (menuNext)
+                                    {
+                                        case 1:
+                                            mc1.Show();
+                                            break;
+                                        case 2:
+                                            Add();
+                                            Console.WriteLine("Элементы добавлены");
+                                            break;
+                                        case 3:
+                                            Delete();
+                                            Console.WriteLine("Элементы удалены");
+                                            break;
+                                        case 4:
+                                            mc1.Sort();
+                                            mc1.Show();
+                                            break;
+                                        case 5:
+                                            mc1.Clear();
+                                            mc1.Show();
+                                            break;
+                                        case 6:
+                                            Console.WriteLine($"Количество:{mc1.Count }");
+                                            break;
+                                    }
+                                }
+
+                            }
+                            break;
+                        #endregion task1
+
+                        #region task2
+                        case 2:
+                            MyNewCollection list1 = new MyNewCollection();
+                            MyNewCollection list2 = new MyNewCollection();
+                            list1.Name = "First";
+                            list2.Name = "Second";
+
+                            Journal jour1 = new Journal();
+                            Journal jour2 = new Journal();
+
+                            list1.CollectionCountChanged += new CollectionHandler(jour1.CollectionCountChanged);
+                            list1.CollectionReferenceChanged += new CollectionHandler(jour1.CollectionReferenceChanged);
+
+                            list2.CollectionCountChanged += new CollectionHandler(jour2.CollectionCountChanged);
+                            list2.CollectionReferenceChanged += new CollectionHandler(jour2.CollectionReferenceChanged);
+
+                            list1.AddRandom();
+                            list1.AddRandom();
+                            list1.AddRandom();
+                            list1.AddRandom();
+
+                            list2.AddRandom();
+                            list2.AddRandom();
+                            list2.AddRandom();
+                            list2.AddRandom();
+
+                            list1.Remove(1);
+                            list2.Remove(2);
+
+                            list1.ChangeValue(2);
+                            list2.ChangeValue(1);
+
+                            list1.Add(new Engine( "ADD", 1111));
+                            Console.WriteLine("В каждую коллекцию добавлено по 4 элемента");
+                            Console.WriteLine("Из первой удален 1, из второй 2");
+                            Console.WriteLine("В первой изменен 2(3 до удаления), во второй 1");
+                            Console.WriteLine("В первую добавлен элемент ADD");
+                            Console.WriteLine();
+
+                            list1.Show();
+                            Console.WriteLine();
+                            list2.Show();
+
+                            Console.WriteLine();
+                            Console.WriteLine("Journal 1:");
+                            Console.WriteLine();
+                            jour1.ToString();
+                            Console.WriteLine();
+                            Console.WriteLine("Journal 2:");
+                            Console.WriteLine();
+                            jour2.ToString();
+
+
+
+                            Console.ReadKey();
+
+                            break;
+                        #endregion task2
+
+                    }
+                }
+            }
+
+        }
+    }
+    public class JournalEntry
+    {
+        public string Name { get; set; }
+        public string ChangeType { get; set; }
+        public string ObjectData { get; set; }
+
+        public JournalEntry(string name, string change, string data)
+        {
+            Name = name;
+            ChangeType = change;
+            ObjectData = data;
+        }
+
+        public override string ToString()
+        {
+            return "Имя коллекции: " + Name + "\nОбъект: " + ObjectData + " - " + ChangeType;
+        }
+    }
+    public class Journal
+    {
+        private List<JournalEntry> journal;
+
+        public Journal()
+        {
+            journal = new List<JournalEntry>();
+        }
+
+        public void CollectionCountChanged(object source, CollectionHandlerEventArgs arg)
+        {
+            JournalEntry jur = new JournalEntry(arg.NameCollection, arg.ChangeType, arg.ObjectData.ToString());
+            journal.Add(jur);
+        }
+        public void CollectionReferenceChanged(object source, CollectionHandlerEventArgs arg)
+        {
+            JournalEntry jur = new JournalEntry(arg.NameCollection, arg.ChangeType, arg.ObjectData.ToString());
+            journal.Add(jur);
+        }
+        public override string ToString()
+        {
+            string str ="";
+            foreach (var item in journal)
+            {
+                Console.WriteLine(item.ToString());
+                str += item.ToString();
+                str += "\n";
+              
+            }
+            return str;
+        }
+    }
+
+  
+}
